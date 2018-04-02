97b2391317262c31450f1249943bcd23ca97ff0f
RozhdestvenskiyMA <RozhdestvenskiyMA@users.noreply.github.com>
2018-04-02 21:31:04 +0300

    initial project version

---
+++ src/main/java/ru/rma/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package ru.rma.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model m) {
+        m.addAttribute("someAttribute", "someValue");
+        return "home";
+    }
+}
